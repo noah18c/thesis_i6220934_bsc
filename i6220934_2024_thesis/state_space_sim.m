2f03655094a08d7c7192d1b789277d5c6b100cfc
noah18c <noahcroes@gmail.com>
2024-04-21 14:53:32 +0200

    sssim plotter done

--- i6220934_2024_thesis/state_space_sim.m
+++ i6220934_2024_thesis/state_space_sim.m
@@ -1,11 +1,17 @@
-function [] = state_space_plotter(state_space, input, maxTime)
+function [] = state_space_sim(state_space, maxTime, input)
     A = state_space.A;
     B = state_space.B;
     C = state_space.C;
     D = state_space.D;
 
     statenum = size(A,1);
+    inputnum = size(B,2);
     outputnum = size(C,1);
+
+    if ~exist('input','var')
+     % if there is no input, then set it to zero
+      input = zeros(inputnum,1);
+    end
 
     x = zeros(statenum,maxTime);
     y = zeros(outputnum,maxTime);
@@ -13,8 +19,8 @@ function [] = state_space_plotter(state_space, input, maxTime)
     y(:,1) = C*x(:,1);
     
     for i=2:maxTime
-        y(:,i) = C*x(:,i-1) + D*;
-        x(:,i) = A*x(:,i-1);
+        y(:,i) = C*x(:,i-1) + D*input;
+        x(:,i) = A*x(:,i-1) + B*input;
     end
     
     plot(0:maxTime-1,y)

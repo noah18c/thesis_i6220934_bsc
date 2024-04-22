6720f667bb4a3fdade4084eafbd856440b79a03a
noah18c <noahcroes@gmail.com>
2024-04-22 23:56:47 +0200

    first nuclear norm model

--- i6220934_2024_thesis/hankelizeM.m
+++ i6220934_2024_thesis/hankelizeM.m
@@ -1,5 +1,5 @@
-function H = hankelizeM(matrix, L)
-    c = matrix(1,1:L);
-    r = matrix(1,L:size(matrix,2));
+function H = hankelizeM(matrix, lags)
+    c = matrix(1,1:lags);
+    r = matrix(1,lags:size(matrix,2));
     H = hankel(c,r);
 end

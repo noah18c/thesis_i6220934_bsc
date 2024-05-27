78eef25f28acb5ea77ca2ff9d1d47513d009c1db
noah18c <72141461+noah18c@users.noreply.github.com>
2024-05-27 18:04:07 +0200

    bugfixed done, testing ready

--- i6220934_2024_thesis/code/ar_cpd_mf.m
+++ i6220934_2024_thesis/code/ar_cpd_mf.m
@@ -1,7 +1,8 @@
-function predictions = ar_cpd(training_series, num_predict, ar_order, cp_order, plotCompare)
+function predictions = ar_cpd_mf(training_series, num_predict, ar_order, cp_order, plotCompare)
     % Function to predict future points in a time series using 
     % Canonical Polyadic Decomposition (CPD) and 
-    % Autoregressive (AR) modeling.
+    % Autoregressive (AR) modeling. Decomposes Hankel tensor using CPD,
+    % trains AR on total reconstructed tensor.
     %
     % Inputs:
     %   training_series - the input time series data for training
@@ -25,33 +26,25 @@ function predictions = ar_cpd(training_series, num_predict, ar_order, cp_order, 
     R = cp_order; % Number of components
     [fac,~] = cpd(H3D, R);
 
-    pred_tcomponents = zeros(num_predict, R);
+    low_rank_tensor = cpdgen(fac);
+
+    % Take average of the anti-diagonal planes
+    tensor_series = serialize3D(low_rank_tensor);
+   
+    model_tcomp = ar(tensor_series, ar_order);
+    predictions = forecast(model_tcomp, tensor_series, num_predict);
+        
 
     if plotCompare
         figure;
-        title("Original vs Serialized components")
+        title("Original vs Low-Rank Approximation")
         plot(training_series);
         hold on;
+        plot(tensor_series);
+        legend({"Original", "Low-rank Approx"});
+        xlabel("Time");
+        ylabel("Value");
+        hold off;
     end
-
-    for tcomp = 1:R
-        tensor_component = cpdgen({fac{1}(:,tcomp),fac{2}(:,tcomp),fac{3}(:,tcomp)});
-        
-        % Reconstruct time series data
-        tensor_series = serialize3D(tensor_component);
-
-        if plotCompare
-            plot(tensor_series);
-            legend({"Original"});
-            xlabel("Time");
-            ylabel("Value");
-            hold off;
-        end
-        
-        model_tcomp = ar(tensor_series, ar_order);
-        pred_tcomponents(:,tcomp) = forecast(model_tcomp, tensor_series, num_predict);
-    end
-  
-    predictions = sum(pred_tcomponents,2);
 
 end
